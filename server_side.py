da1d3dd6baf5c75f2de5c31701e6960d0072f773
ilan <ilanydg@gmail.com>
2025-08-26 22:36:17 +0300

    modified search

--- server_side.py
+++ server_side.py
@@ -12,25 +12,33 @@ def run_local_command(command):
         print(f"Command '{command}' returned non-zero exit status {e.returncode}")
         print(f"Error output: {e.stderr}")
 
-def grep_lines_counter(keyword, path_to_file):
-    try:
-        return run_local_command(f'grep "{keyword}" {path_to_file} | wc -l')
+# def grep_lines_counter(keyword, path_to_file):
+#     try:
+#         return run_local_command(f'grep "{keyword}" {path_to_file} | wc -l')
     
-    except Exception as e:
-        return f"Error: {e}"
+#     except Exception as e:
+#         return f"Error: {e}"
 
 def main():
     data = {}
-    info_count = grep_lines_counter("INFO", "/var/log/syslog")
-    warn_count = grep_lines_counter("WARN", "/var/log/syslog")
-    error_count = grep_lines_counter("ERROR", "/var/log/syslog")
-    data["INFO"] = int(info_count[0])
-    data["WARN"] = int(warn_count[0])
-    data["ERROR"] = int(error_count[0])
+    info_count = 0
+    warn_count = 0
+    error_count = 0
+    with open("/var/log/syslog", "r") as log:
+        for line in log:
+            if "INFO" in line:
+                info_count += 1
+            elif "WARN" in line:
+                warn_count += 1
+            elif "ERROR" in line:
+                error_count += 1
+
+    data["INFO"] = info_count
+    data["WARN"] = warn_count
+    data["ERROR"] = error_count
     data["timestamp"] = int(time.time())
     data = json.dumps(data)
     print(data)
-    #return data
 
 if __name__ == "__main__":
     main()
